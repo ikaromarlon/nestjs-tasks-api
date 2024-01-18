dded5ce45e0feddc7891cd8e01311dfd9ad5b949
Icaro Silva <ikaromarlon@gmail.com>
2024-01-18 18:46:38 +0100

    feat: add error validation to task controller

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,34 +1,47 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, Delete, Get, HttpCode, NotFoundException, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private taskService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 
   @Post()
-  async create(@Body() createTask: CreateTaskDto) {
-    return this.taskService.create(createTask);
+  async create(@Body() data: CreateTaskDto) {
+    try {
+      return this.tasksService.create(data);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Task already exists');
+      }
+      throw error;
+    }
   }
 
   @Get()
   async findAll() {
-    return this.taskService.findAll();
+    return this.tasksService.findAll();
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return this.taskService.findOne(id);
+    const task = await this.tasksService.findOne(id);
+    if (!task) throw new NotFoundException('Task does not exist!');
+    return task;
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.taskService.update(id, updateTaskDto);
+  async update(@Param('id') id: string, @Body() body: UpdateTaskDto) {
+    const task = await this.tasksService.update(id, body);
+    if (!task) throw new NotFoundException('Task does not exist!');
+    return task;
   }
 
   @Delete(':id')
   @HttpCode(204)
   async delete(@Param('id') id: string) {
-    return this.taskService.delete(id)
+    const task = await this.tasksService.delete(id);
+    if (!task) throw new NotFoundException('Task does not exist!');
+    return task;
   }
 }
